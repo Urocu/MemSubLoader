04e481cb7669fa661edef39b15835c95bbb73656
Urocu <103362793+Urocu@users.noreply.github.com>
2023-10-16 21:15:50 +0200

    New Subtitle class+ remade file handling

--- includes/MemSubLoader.hpp
+++ includes/MemSubLoader.hpp
@@ -11,6 +11,9 @@
 # include <string>
 # include <vector>
 # include <stdio.h>
+# include <chrono>
+# include <thread>
+# include <sstream>
 # define GAME 1
 # define TRANSLATION 2
 # define START 3
@@ -24,6 +27,7 @@ extern HWND configHWND;
 extern wchar_t gamePath[MAX_PATH];
 extern wchar_t filePath[MAX_PATH];
 
+
 // Main
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow);
 
@@ -34,8 +38,30 @@ void CreateConfigWindow(HWND parent);
 LRESULT CALLBACK ConfigWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 // Utilities
-void findAddress(uintptr_t &address, int offset, HANDLE hProcess);
 bool OpenFileExplorer(HWND hwnd, wchar_t *filePath, int filePathSize, int button);
+
+class Subtitles
+{
+private:
+    uintptr_t bAddress_audio;
+    std::vector <int> offset_audio;
+    uintptr_t bAddress_play;
+    std::vector <int> offset_play;
+    void findAddress(uintptr_t &address, int offset, HANDLE hProcess);
+public:
+    uintptr_t address_audio;
+    uintptr_t address_play;
+    int AudioID;
+    int lastAudioID;
+    bool is_playing;
+    std::vector <int> ID;
+    std::vector <std::wstring> Text;
+
+    void search_memory(HANDLE hProcess);
+    bool check_audio(HANDLE hProcess);
+    void file_memory(std::wifstream& file);
+    void file_text(std::wifstream& file);
+};
 
 // Game
 void game_start(PROCESS_INFORMATION pi);
--- srcs/game.cpp
+++ srcs/game.cpp
@@ -3,61 +3,61 @@
 // Used when the game starts
 void game_start(PROCESS_INFORMATION pi)
 {
-	SIZE_T bytesRead;
-	int audID;
-	uintptr_t baseaddress;
-	uintptr_t addressToRead;
-	std::vector <std::wstring> Text;
-	std::wstring ws( filePath );
-	std::string SfilePath( ws.begin(), ws.end() );
-	std::wifstream subfile;
-
-	subfile.open(SfilePath); // Opens file containing base address, offsets and text for subtitles
-	int num;
-	int offset[6];
+    int num;
+	std::vector <Subtitles> Sub;
+	std::wifstream subfile(filePath);
 	if(subfile.is_open() && !subfile.eof())
 	{
-		subfile>>baseaddress;
-		subfile>>num;
-		for(int i = 0; i<num;i++)
-		   subfile>>offset[i];
-		while(getline(subfile, ws))
-		{
-			Text.push_back(ws);
-		}
+	    subfile >> num;
+	    for(int i = 0; i <num; i++)
+        {
+            Sub.push_back(Subtitles());
+            Sub[i].file_memory(subfile);
+        }
+        for(int i = 0; i < num; i++)
+        {
+            Sub[i].file_text(subfile);
+        }
+
 	}
 	else
 		SetWindowText(subtitles, L"File failed to open");
 	subfile.close();
 
-	int lastID = 0;
 	DWORD Width = 0;
 	DWORD Height = 0;
+	bool is;
+	const std::chrono::milliseconds frame_duration(1000 / 60);
 	while (WaitForSingleObject( pi.hProcess, 0 ) == WAIT_TIMEOUT)
 	{
-		// Read memory of audioID's address
-		if (ReadProcessMemory(pi.hProcess, (LPCVOID)addressToRead, &audID, sizeof(audID), &bytesRead) && lastID!=audID && audID <Text.size() && audID >0)
-		{
-			SetWindowText(subtitles, Text[audID].c_str());
-			lastID=audID;
-		}
+	    auto start_time = std::chrono::high_resolution_clock::now();
+	    for(int i = 0; i < num; i++)
+        {
+            is = false;
+            Sub[i].search_memory(pi.hProcess);
+            if(Sub[i].check_audio(pi.hProcess))
+                is = true;
+        }
+        if(!is)
+            SetWindowText(subtitles, L"");
 		// process messages, otherwise the software will freeze
+        auto end_time = std::chrono::high_resolution_clock::now();
+        auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
 		MSG msg = { };
-		while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)|| elapsed_time < frame_duration)
 		{
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
+			end_time = std::chrono::high_resolution_clock::now();
+            elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+            std::this_thread::sleep_for((std::chrono::milliseconds)10);
 		}
 
-		addressToRead = baseaddress;
-		for( int i = 0; i < num; i++)
-			findAddress(addressToRead, offset[i], pi.hProcess);
-		// If width and height of the screen changed, resize and reposition the subtitles window
 		if(Width != GetSystemMetrics(SM_CXSCREEN) && Height != GetSystemMetrics(SM_CYSCREEN))
 		{
 			Width = GetSystemMetrics(SM_CXSCREEN);
 			Height = GetSystemMetrics(SM_CYSCREEN);
-			SetWindowPos(subtitlesWin, HWND_TOPMOST, 0, Height-100, Width, 100, 0);
+			SetWindowPos(subtitlesWin, HWND_TOPMOST, 0, Height-100, Width = GetSystemMetrics(SM_CXSCREEN), 100, 0);
 			SetWindowPos(subtitles, 0, 0, 0, Width, 100, 0);
 		}
 
